0a259f073c8118189c27cace460eb604c4cbf4ce
MateoAOrdonez <87553857+MateoAOrdonez@users.noreply.github.com>
2022-02-03 16:32:08 -0500

    Header v1

---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import './header.css';
+
+const header = () => {
+    const x1 = "<";
+    const x2 = "MO";
+    const x3 = "/>";
+    return (
+        <Grid>
+            <header className="HeaderPrincipal">
+                <div className="HeaderNombre">
+                    <h1 className="HeaderNombre1">{x1}</h1>
+                    <h1 className="HeaderNombre2">{x2}</h1>
+                    <h1 className="HeaderNombre3">{x3}</h1>
+                </div>
+                <div className="HeaderMenu">
+                    <h3>Incio</h3>
+                    <h3>Perfil</h3>
+                    <h3>Portafolio</h3>
+                </div>
+            </header>
+        </Grid>
+    )
+}
+
+export default header;
