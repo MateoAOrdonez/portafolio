8284a51ec7dd0f81b1b6f429e1dd805ed044f455
MateoAOrdonez <87553857+MateoAOrdonez@users.noreply.github.com>
2022-02-07 19:24:58 -0500

    Ajuste color

--- src/components/presentacion/presentacion.js
+++ src/components/presentacion/presentacion.js
@@ -8,7 +8,7 @@ const presentacion = () => {
             <img src={mainLogo} alt="Logo" className="Logo"></img>
             <div className="Nombre">
                 <h1 style={{color: "white", margin: "5px"}}>Hola</h1>
-                <h1 style={{color: "#B3834D", margin: "5px"}}>Soy Mateo Ordoñez</h1>
+                <h1 style={{color: "#4369D9", margin: "5px"}}>Soy Mateo Ordoñez</h1>
                 <h2 style={{color: "white", margin: "10px"}}>Ingeniero Multimedia</h2>
             </div>
         </div>
